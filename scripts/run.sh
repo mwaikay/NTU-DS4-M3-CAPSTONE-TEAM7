64553745fab4f8c5d66334186fdaf6ea1ac48b78
Isaac9991 <isaacchin100@yahoo.com.sg>
2026-04-21 16:38:05 +0800

    updated setup and run scripts for easier readibility

---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+echo "Activating virtual environment..."
+source myenv/bin/activate
+
+#temp run jupyter notebook only, will run main.py later
+echo "Running Jupyter Notebook..."
+jupyter notebook notebook/eda.ipynb
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,17 @@
+echo "Going to parent directory..."
+cd ..
+
 echo "Creating virtual environment..."
 python3 -m venv myenv
 
 echo "Activating virtual environment..."
 source myenv/bin/activate
 
+echo "Upgrading pip..."
+pip install --upgrade pip
+
 echo "Installing dependencies..."
 pip install -r requirements.txt
+
+echo "Setup complete"
 
